8c440a3f51aa4d420426b6056ff8594af3bf4bce
Dumb <74306976+DumbGames@users.noreply.github.com>
2022-01-09 12:48:17 +0530

    Update Spike.gd

--- Platformer Pack/source/Spike.gd
+++ Platformer Pack/source/Spike.gd
@@ -1,8 +1,5 @@
 extends Area2D
 
-onready var anim = $AnimationPlayer
-
-
 func _on_Spike_body_entered(body):
 	#if body_is_in_group("player"):
 		#body.die()  (create a die function in the player script)
